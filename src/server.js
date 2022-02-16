056dcc3fd6782835fd13c7e57e55dd8c5d1e6b80
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2022-02-16 06:01:18 +0100

    sd

--- src/server.js
+++ src/server.js
@@ -1,3 +1,20 @@
 import express from "express";
+import listEndpoints from "express-list-endpoints";
+import cors from "cors";
+import mongoose from "mongoose";
 
 const server = express();
+const port = process.env.PORT || 3001;
+
+server.use(cors());
+server.use(express.json());
+
+mongoose.connect(process.env.MONGO_CONNECTION);
+
+mongoose.connection.on("connected", () => {
+  console.log("Successfully connected to Mongo!");
+  server.listen(port, () => {
+    console.table(listEndpoints(server));
+    console.log("Server runnning on port: ", port);
+  });
+});
