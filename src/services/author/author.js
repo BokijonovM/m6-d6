0d975639272e902a1277f222708c2eea133aa733
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2022-02-17 17:00:23 +0100

    updated

--- src/services/author/author.js
+++ src/services/author/author.js
@@ -1,5 +1,7 @@
 import express from "express";
 import AuthorModel from "./schema.js";
+import BlogsModel from "../blog/schema.js";
+import createHttpError from "http-errors";
 
 const authorsRouter = express.Router();
 
@@ -68,4 +70,87 @@ authorsRouter.delete("/:authorId", async (req, res, next) => {
     next(error);
   }
 });
+
+////
+////
+// *********************** users
+////
+////
+
+authorsRouter.post("/:authorId/user", async (req, res, next) => {
+  try {
+    const updatedAuthor = await AuthorModel.findByIdAndUpdate(
+      req.params.authorId,
+      { $push: { user: req.body } },
+      { new: true }
+    );
+    if (updatedAuthor) {
+      res.send(updatedAuthor);
+    } else {
+      next(createHttpError(404, `Author with id ${authorId} not found!`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+authorsRouter.get("/:authorId/user", async (req, res, next) => {
+  try {
+    const user = await AuthorModel.findById(req.params.authorId);
+    if (user) {
+      res.send(user.user);
+    } else {
+      next(
+        createHttpError(404, `User with Id ${req.params.authorId} not found!`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+authorsRouter.get("/:authorId/user/:userId", async (req, res, next) => {
+  try {
+    const author = await AuthorModel.findById(req.params.authorId);
+    const findUser = author.user?.find(
+      findUser => findUser._id.toString() === req.params.userId
+    );
+    if (findUser) {
+      res.send(findUser);
+    } else {
+      next(
+        createHttpError(404, `Author with Id ${req.params.authorId} not found!`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+////
+////
+// *********************** comment
+////
+////
+
+authorsRouter.post(
+  "/:authorId/user/:userId/comment",
+  async (req, res, next) => {
+    try {
+      const updatedAuthor = await AuthorModel.findByIdAndUpdate(
+        req.params.authorId,
+        { $push: { "user.$.comment": req.body } },
+        { new: true }
+      );
+      if (updatedAuthor) {
+        res.send(updatedAuthor);
+      } else {
+        next(createHttpError(404, `Author with id ${authorId} not found!`));
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 export default authorsRouter;
